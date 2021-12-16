2bd22e849420ec82089818188b16378f9171de53
Gijsbert te Paske <gstpsk@outlook.com>
2021-12-16 22:29:10 +0100

    Added sqlite3 database and implemented an easy to use error check (may be bad practice because it just log.Fatals on any error atm but its fine for now lol)

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"os"
+
 	"github.com/go-zepto/zepto"
 	"github.com/gstpsk/Plentor/controllers"
 	"github.com/gstpsk/Plentor/util"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	// Load the config file
 	util.LoadConfig()
+
+	// Initialize the database
+	os.Remove("./db/dev.db")
+	db, err := sql.Open("sqlite3", "./db/dev.db")
+	util.Check(err)
+	defer db.Close() // good practice mate
+
+	sqlStmt := `
+		create table users (id integer not null primary key, username text not null, hash text not null)
+	
+	`
+	_, err = db.Exec(sqlStmt)
+	util.Check(err)
+
+	//tx, err := db.Begin()
+	util.Check(err)
 
 	// Create Zepto
 	z := zepto.NewZepto()
