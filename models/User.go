90dbf6350f9d22f494b2a08b3bd2c5bb92f39626
Gijsbert te Paske <gstpsk@outlook.com>
2021-12-23 20:58:51 +0100

    a fucking space

--- models/User.go
+++ models/User.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id    string `json:"_id, omitempty" bson:"_id,omitempty"`
+	Id    string `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 	Hash  string `json:"hash"`
